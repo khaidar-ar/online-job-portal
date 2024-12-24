edcb0ec00f09b1b0cb9c3b8c67b58498557b4770
khaidar-ar <96arrijal@gmail.com>
2024-12-24 19:37:31 +0700

    initial commit

---
+++ server-app/src/test/java/com/job_portal/server_app/OnlineJobPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.job_portal.server_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineJobPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
